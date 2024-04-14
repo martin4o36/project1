d52114b6ff175ef2b86fbe0a92a70db4eff22697
martin4o36 <136126090+martin4o36@users.noreply.github.com>
2024-04-14 22:37:46 +0300

    Update Services.java

--- src/main/java/app/services/Services.java
+++ src/main/java/app/services/Services.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import app.model.AirportFilter;
+
 @AllArgsConstructor
 @Service
 public class Services {
@@ -18,5 +20,13 @@ public class Services {
     public List<AirportDto> getAllAirports() {
         List<Airport> airports = airportRepository.findAll();
         return airports.stream().map(AirportMapper::mapToAirportDto).collect(Collectors.toList());
+    }
+
+    public List<?> filterAirportsWithCountryAsRoot(AirportFilter airportFilter) {
+
+    }
+
+    public List<?> filterAirportsWithFilter(AirportFilter airportFilter) {
+
     }
 }
